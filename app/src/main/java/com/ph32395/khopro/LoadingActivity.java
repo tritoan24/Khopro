e4a41af8efddf545aedef29b53e2788f5ff548b2
tritoan24 <toanntph32395@fpt.edu.vn>
2023-11-24 01:29:39 +0700

    tritoanadd ngày 24/11

--- app/src/main/java/com/ph32395/khopro/LoadingActivity.java
+++ app/src/main/java/com/ph32395/khopro/LoadingActivity.java
@@ -2,7 +2,12 @@ package com.ph32395.khopro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class LoadingActivity extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class LoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+
+        ImageView bunchaImage = findViewById(R.id.buncha_image);
+        Animation rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        bunchaImage.startAnimation(rotateAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LoadingActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
     }
 }
