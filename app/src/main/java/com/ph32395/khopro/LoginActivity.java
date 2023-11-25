16c3366b95df0f0e33ffe4565fbac1b2f21ce11f
tritoan24 <toanntph32395@fpt.edu.vn>
2023-11-25 16:56:55 +0700

    tritoanadd ngày 25/11

--- app/src/main/java/com/ph32395/khopro/LoginActivity.java
+++ app/src/main/java/com/ph32395/khopro/LoginActivity.java
@@ -2,13 +2,27 @@ package com.ph32395.khopro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btndangnhap_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
