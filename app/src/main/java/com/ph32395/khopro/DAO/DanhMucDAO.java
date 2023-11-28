ecccdb33ead0e28bf04eb1ba751cfc0bfb2db54c
tritoan24 <toanntph32395@fpt.edu.vn>
2023-11-28 10:32:21 +0700

    tritoan chỉnh lại database và chức năng đổi mk_BanAn

--- app/src/main/java/com/ph32395/khopro/DAO/DanhMucDAO.java
+++ app/src/main/java/com/ph32395/khopro/DAO/DanhMucDAO.java
@@ -1,4 +1,54 @@
 package com.ph32395.khopro.DAO;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.ph32395.khopro.Database.DbHelper;
+import com.ph32395.khopro.Model.BanAn;
+import com.ph32395.khopro.Model.DanhMucMonAn;
+
+import java.util.ArrayList;
+
 public class DanhMucDAO {
+    DbHelper dbHelper;
+
+    public DanhMucDAO(Context context){
+        dbHelper = new DbHelper(context);
+    }
+
+    public ArrayList<DanhMucMonAn> getListDanhMuc(){
+        ArrayList<DanhMucMonAn> list = new ArrayList<>();
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("select * from DanhMuc", null);
+        if (cursor.getCount() != 0){
+            cursor.moveToFirst();
+            do{
+                list.add(new DanhMucMonAn(cursor.getInt(0), cursor.getString(1)));
+            }while (cursor.moveToNext());
+        }
+        return list;
+    }
+
+    public long insert(DanhMucMonAn danhMuc){
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("id_DanhMuc", danhMuc.getId_DanhMuc());
+        values.put("tenDanhMuc", danhMuc.getTenDanhMuc());
+        return db.insert("DanhMuc", null, values);
+    }
+
+    public long delete(int id){
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        return db.delete("DanhMuc","id_DanhMuc = ?",new String[]{String.valueOf(id)});
+
+    }
+
+    public long update(DanhMucMonAn danhMuc){
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("tenDanhMuc", danhMuc.getTenDanhMuc());
+        return db.update("DanhMuc",values,"id_DanhMuc = ?",new String[]{String.valueOf(danhMuc.getId_DanhMuc())});
+    }
 }
